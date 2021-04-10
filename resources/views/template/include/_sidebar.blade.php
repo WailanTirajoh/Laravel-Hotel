becdaa71a66af3832f62c2fd0cbac7fb0f48c525
Wailan Tirajoh <wailantirajoh@gmail.com>
2021-04-11 01:57:52 +0900

    laravel way route name untuk active link sidebar

--- resources/views/template/include/_sidebar.blade.php
+++ resources/views/template/include/_sidebar.blade.php
@@ -9,7 +9,7 @@
         <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
             <li class="nav-item">
                 <a href="/dashboard"
-                    class="nav-link py-3 border-bottom myBtn @if (stripos($_SERVER['REQUEST_URI'], 'dashboard' ) !==false) active @endif"
+                    class="nav-link py-3 border-bottom myBtn {{ Route::currentRouteName() == 'dashboard' ? 'active' : '' }}"
                     data-bs-toggle="tooltip" data-bs-placement="right" title="Dashboard">
                     <svg width="25" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -18,9 +18,8 @@
                 </a>
             </li>
             <li>
-                <a href="/payment"
-                    class="nav-link py-3 border-bottom myBtn @if (stripos($_SERVER['REQUEST_URI'], 'payment' ) !==false) active @endif"
-                    title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Payment">
+                <a href="/payment" class="nav-link py-3 border-bottom myBtn {{ Route::currentRouteName() == 'payment' ? 'active' : '' }}
+                    title="" data-bs-toggle=" tooltip" data-bs-placement="right" data-bs-original-title="Payment">
                     <svg width="25" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -29,8 +28,8 @@
             </li>
             <li>
                 <div class="dropend">
-                    <a class="nav-link py-3 border-bottom myBtn @if (stripos($_SERVER['REQUEST_URI'], 'room') !==false) active @elseif(stripos($_SERVER['REQUEST_URI'], 'type') !==false) active @endif"
-                        data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link py-3 border-bottom myBtn {{ Route::currentRouteName() == 'room' ? 'active' : '' }} {{ Route::currentRouteName() == 'type' ? 'active' : '' }}
+                        data-bs-toggle=" dropdown" aria-expanded="false">
                         <svg width="25" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
@@ -46,8 +45,8 @@
             </li>
             <li>
                 <div class="dropend">
-                    <a class="nav-link py-3 border-bottom myBtn @if (stripos($_SERVER['REQUEST_URI'], 'customer') !==false) active @elseif(stripos($_SERVER['REQUEST_URI'], 'user') !==false) active @endif"
-                        data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link py-3 border-bottom myBtn {{ Route::currentRouteName() == 'customer' ? 'active' : '' }} {{ Route::currentRouteName() == 'user' ? 'active' : '' }}
+                        data-bs-toggle=" dropdown" aria-expanded="false">
                         <svg width="25" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
