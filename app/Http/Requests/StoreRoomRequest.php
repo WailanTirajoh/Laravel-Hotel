96932959cdb38c12dc6eaad76bb7b215f98866c8
Wailan Tirajoh <wailantirajoh@gmail.com>
2021-04-11 21:36:14 +0900

    tambah store room request dan update customer request

---
+++ app/Http/Requests/StoreRoomRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRoomRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if(!empty($this->room->id))
+        {
+            return [
+                'type_id' => 'required',
+                'number' => 'required|unique:rooms,number,'.$this->room->id,
+                'capacity' => 'required|numeric',
+                'price' => 'required|numeric',
+                'view' => 'required|max:255'
+            ];
+        }
+        return [
+            'type_id' => 'required',
+            'number' => 'required|unique:rooms,number',
+            'capacity' => 'required|numeric',
+            'price' => 'required|numeric',
+            'view' => 'required|max:255'
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdateCustomerRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if ($this->user->role == "Customer") {
+            return [
+                'name' => 'required',
+                'email' => 'required|unique:users,email,' . $this->user->id,
+                'role' => 'required|in:Customer',
+            ];
+        }
+        return [
+            'name' => 'required',
+            'email' => 'required|unique:users,email,' . $this->user->id,
+            'role' => 'required|in:Super,Admin',
+        ];
+    }
+}
