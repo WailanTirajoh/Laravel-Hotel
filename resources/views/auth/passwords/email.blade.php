d85ff04974d06cfe0219fb1561ffd81141b2aedb
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2023-11-25 14:13:09 +0700

    feat: add session status

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -44,6 +44,11 @@
 
                         <div class="row">
                             <div class="col-lg-12">
+                                @if (session('status'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
                                 <form onsubmit="return disableButton()" method="POST"
                                     action="{{ route('password.email') }}"
                                     style="display: flex; flex-direction: column; gap: 8px;"
