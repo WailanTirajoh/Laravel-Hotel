50c44bfc97c64b97218f19813814309473c63b29
Cristhian Recalde <cdrecalde@gmail.comm>
2024-06-26 00:18:17 -0500

    refactor: update currency format in Helper class

--- resources/views/transaction/reservation/confirmation.blade.php
+++ resources/views/transaction/reservation/confirmation.blade.php
@@ -14,25 +14,25 @@
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="row mb-3">
-                                <label for="room_number" class="col-sm-2 col-form-label">Room</label>
+                                <label for="room_number" class="col-sm-2 col-form-label">Habitacion</label>
                                 <div class="col-sm-10">
                                     <input type="text" class="form-control" id="room_number" name="room_number" placeholder="col-form-label" value="{{ $room->number }} " readonly>
                                 </div>
                             </div>
                             <div class="row mb-3">
-                                <label for="room_type" class="col-sm-2 col-form-label">Type</label>
+                                <label for="room_type" class="col-sm-2 col-form-label">Tipo</label>
                                 <div class="col-sm-10">
                                     <input type="text" class="form-control" id="room_type" name="room_type" placeholder="col-form-label" value="{{ $room->type->name }} " readonly>
                                 </div>
                             </div>
                             <div class="row mb-3">
-                                <label for="room_capacity" class="col-sm-2 col-form-label">Capacity</label>
+                                <label for="room_capacity" class="col-sm-2 col-form-label">Capacidad</label>
                                 <div class="col-sm-10">
                                     <input type="text" class="form-control" id="room_capacity" name="room_capacity" placeholder="col-form-label" value="{{ $room->capacity }} " readonly>
                                 </div>
                             </div>
                             <div class="row mb-3">
-                                <label for="room_price" class="col-sm-2 col-form-label">Price / Day</label>
+                                <label for="room_price" class="col-sm-2 col-form-label">Precio / Día</label>
                                 <div class="col-sm-10">
                                     <input type="text" class="form-control" id="room_price" name="room_price" placeholder="col-form-label" value="{{ Helper::convertToRupiah($room->price) }}" readonly>
                                 </div>
@@ -43,39 +43,39 @@
                             <form id="paymentForm" method="POST" action="{{ route('transaction.reservation.payDownPayment', ['customer' => $customer->id, 'room' => $room->id]) }}">
                                 @csrf
                                 <div class="row mb-3">
-                                    <label for="check_in" class="col-sm-2 col-form-label">Check In</label>
+                                    <label for="check_in" class="col-sm-2 col-form-label">Ingreso</label>
                                     <div class="col-sm-10">
                                         <input type="date" class="form-control" id="check_in" name="check_in" placeholder="col-form-label" value="{{ $stayFrom }}" readonly>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="check_out" class="col-sm-2 col-form-label">Check Out</label>
+                                    <label for="check_out" class="col-sm-2 col-form-label">Salida</label>
                                     <div class="col-sm-10">
                                         <input type="date" class="form-control" id="check_out" name="check_out" placeholder="col-form-label" value="{{ $stayUntil }}" readonly>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="how_long" class="col-sm-2 col-form-label">Total Day</label>
+                                    <label for="how_long" class="col-sm-2 col-form-label">Precio por dia</label>
                                     <div class="col-sm-10">
                                         <input type="text" class="form-control" id="how_long" name="how_long" placeholder="col-form-label" value="{{ $dayDifference }} {{ Helper::plural('Day', $dayDifference) }} " readonly>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="total_price" class="col-sm-2 col-form-label">Total Price</label>
+                                    <label for="total_price" class="col-sm-2 col-form-label">Precio Total</label>
                                     <div class="col-sm-10">
                                         <input type="text" class="form-control" id="total_price" name="total_price" placeholder="col-form-label" value="{{ Helper::convertToRupiah(Helper::getTotalPayment($dayDifference, $room->price)) }} " readonly>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="minimum_dp" class="col-sm-2 col-form-label">Minimum DP</label>
+                                    <label for="minimum_dp" class="col-sm-2 col-form-label">Monto minimo $</label>
                                     <div class="col-sm-10">
                                         <input type="text" class="form-control" id="minimum_dp" name="minimum_dp" placeholder="col-form-label" value="{{ Helper::convertToRupiah($downPayment) }} " readonly>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="downPayment" class="col-sm-2 col-form-label">Payment</label>
+                                    <label for="downPayment" class="col-sm-2 col-form-label">Pago</label>
                                     <div class="col-sm-10">
-                                        <input type="text" class="form-control @error('downPayment') is-invalid @enderror" id="downPayment" name="downPayment" placeholder="Input payment here" value="{{ old('downPayment') }}">
+                                        <input type="text" class="form-control @error('downPayment') is-invalid @enderror" id="downPayment" name="downPayment" placeholder="Ingresar Pago" value="{{ old('downPayment') }}">
                                         @error('downPayment')
                                         <div class="text-danger mt-1">
                                             {{ $message }}
@@ -87,7 +87,7 @@
                                     <div class="col-sm-2"></div>
                                     <div class="col-sm-10" id="showPaymentType"></div>
                                 </div>
-                                <button type="submit" class="btn btn-primary float-end" id="payButton">Pay DownPayment</button>
+                                <button type="submit" class="btn btn-primary float-end" id="payButton">Pago</button>
                             </form>
                         </div>
                     </div>
@@ -148,7 +148,7 @@
 @section('footer')
 <script>
     $('#downPayment').keyup(function() {
-        $('#showPaymentType').text('Rp. ' + parseFloat($(this).val(), 10).toFixed(2).replace(
+        $('#showPaymentType').text('$. ' + parseFloat($(this).val(), 10).toFixed(2).replace(
                 /(\d)(?=(\d{3})+\.)/g, "$1.")
             .toString());
     });
